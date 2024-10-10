7ca06b4659b707949a2f43fcd135ffe5f9fdda5a
Alagusoundarya Manivasagam <alagusoundarya1995@gmail.com>
2024-10-10 05:52:01 -0500

    Encapsulation Homework

--- Module-303/Module-303/src/homework/Glab303_9_1/EncapsulationExample303_9_1.java
+++ Module-303/Module-303/src/homework/Glab303_9_1/EncapsulationExample303_9_1.java
@@ -1,4 +1,15 @@
 package homework.Glab303_9_1;
 
 public class EncapsulationExample303_9_1 {
+    public static void main(String[] args) {
+        HumanBeing303_9_1 h1 = new HumanBeing303_9_1();
+        h1.setHeight(1.65f);
+        h1.setWeight(68);
+        h1.setBmi(calculateBmi(h1));
+        System.out.println("Person has" + h1.getWeight() + " kgs and is " + h1.getHeight() +
+                " meters in height, which results in BMIof" + h1.getBmi());
+    }
+    public static float calculateBmi(HumanBeing303_9_1 h1) {
+        return h1.getWeight() / (h1.getHeight() * h1.getHeight());
+    }
 }
--- Module-303/Module-303/src/homework/Glab303_9_1/HumanBeing303_9_1.java
+++ Module-303/Module-303/src/homework/Glab303_9_1/HumanBeing303_9_1.java
@@ -1,4 +1,40 @@
 package homework.Glab303_9_1;
 
 public class HumanBeing303_9_1 {
+    private float weight;
+    private float height;
+    private float bmi;
+
+    public HumanBeing303_9_1(float weight, float height, float bmi) {
+        this.weight = weight;
+        this.height = height;
+        this.bmi = bmi;
+    }
+    public HumanBeing303_9_1() {
+
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getBmi() {
+        return bmi;
+    }
+
+    public void setBmi(float bmi) {
+        this.bmi = bmi;
+    }
 }
