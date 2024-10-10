991a67c6a1759fe009433668d7ce23c58a71a8e1
Alagusoundarya Manivasagam <alagusoundarya1995@gmail.com>
2024-10-10 05:32:34 -0500

    Basic Inheritance Example Homework

---
+++ Module-303/Module-303/src/homework/Glab303_9_2/Surgeon303_9_2.java
@@ -0,0 +1,8 @@
+package homework.Glab303_9_2;
+
+public class Surgeon extends Doctor {
+    void Surgeon_Details(){
+        System.out.println("Surgeon Details....");
+        System.out.println(Department = "cardio");
+    }
+}
